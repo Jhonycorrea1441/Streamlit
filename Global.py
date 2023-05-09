bf19f58ec307eb073e9a26dbcbf412b930cdce53
Jhony Correa Marín <87825878+Jhonycorrea1441@users.noreply.github.com>
2023-05-08 19:21:33 -0500

    Update Global.py

--- Global.py
+++ Global.py
@@ -1,53 +1,64 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
-from urllib.error import URLError
+import plotly.express as px
 
+
+#Leer datos
+df_origin = pd.read_csv('fifa23players.csv')
+
+with st.sidebar:
+    nationality = st.multiselect('Nacionalidad',sorted(df_origin['Nationality'].unique()))
+    team = st.multiselect('Equipo', sorted(df_origin['Club Name'].unique()))
+    is_national = st.checkbox('Selección Nacional')
+
+def filter_data(df, nationality, team, is_national):
+    df_copy = df.copy()
+
+    if len(nationality) > 0:
+        df_copy = df_copy[df_copy['Nationality'].isin(nationality)]
+    if len(team) > 0:
+        df_copy = df_copy[df_copy['Club Name'].isin(team)]
+
+    if is_national == True:
+        df_copy = df_copy[df_copy['National Team Name'] != '-']
     
-st.set_page_config(page_title="DataFrame Economia Mundial", page_icon="📊")
+    return df_copy
 
-st.markdown("Economia Mundial")
-st.sidebar.header("Global")
+df_ = filter_data(df_origin, nationality, team, is_national)
+st.title("Fifa 2023")
+st.subheader("Análisis de Equipos")
 
-@st.cache_data
-def get_UN_data():
-    AWS_BUCKET_URL = "http://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-    df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-    return df.set_index("Region")
+total_jugadores = len(df_)
+rating_medio = df_['Overall'].mean()
+valor_medio = df_['Value(in Euro)'].mean()
+
+col1, col2, col3 = st.columns(3)
+col1.metric("# Jugadores", f"{total_jugadores:,.0f}")
+col2.metric("Rating Medio", f"{rating_medio:,.1f}")
+col3.metric("Valor $ Medio", f"{valor_medio:,.0f}")
 
 
-try:
-    df = get_UN_data()
-    countries = st.multiselect(
-        "Elige un pais", list(df.index), ["China", "United States of America"]
-    )
-    if not countries:
-        st.error("Please select at least one country.")
-    else:
-        data = df.loc[countries]
-        data /= 1000000.0
-        st.write("### Gross Agricultural Production ($B)", data.sort_index())
 
-        data = data.T.reset_index()
-        data = pd.melt(data, id_vars=["index"]).rename(
-            columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-        )
-        chart = (
-            alt.Chart(data)
-            .mark_area(opacity=0.3)
-            .encode(
-                x="year:T",
-                y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                color="Region:N",
-            )
-        )
-        st.altair_chart(chart, use_container_width=True)
-except URLError as e:
-    st.error(
-        """
-        **This demo requires internet access.**
-        Connection error: %s
-    """
-        % e.reason
-    )
+def get_team_statistics(df):
+    radar_columns = ['Pace Total','Shooting Total','Passing Total',
+                'Dribbling Total','Defending Total','Physicality Total']
+    metrics = []
+    for metric in radar_columns:
+        metrics.append(df_[metric].mean())
+    
+    return pd.DataFrame(dict(metrics=metrics, theta=radar_columns))
+
+radar_fig = px.line_polar(get_team_statistics(df_), r='metrics', theta='theta', line_close=True)
+
+radar_fig.update_layout(
+  polar=dict(
+    radialaxis=dict(
+      visible=True,
+      range=[0, 100]
+    )),
+  showlegend=False
+)
+st.plotly_chart(radar_fig)
+
+st.dataframe(df_)
 
